b39ef97404a708cdad83b5b871c42e375973145b
Dmitry Shumilin <infernozzz@yandex.ru>
2021-01-11 14:18:16 +0300

    add PhpSpreadsheet

--- event-platform-statistics.php
+++ event-platform-statistics.php
@@ -9,7 +9,11 @@
  */
 use EPStatistics\Main;
 
-require_once __DIR__.'/autoload.php';
+require_once __DIR__.'/event-platform-statistics-autoload.php';
+
+if (!file_exists(__DIR__.'/vendor/autoload.php')) exec('composer install');
+
+require_once __DIR__.'/vendor/autoload.php';
 
 new Main(
     plugin_dir_path(__FILE__),
