50becd28d009dce1633bf6ea798a8358aab5e50f
Dmitry Shumilin <infernozzz@yandex.ru>
2021-01-22 14:03:26 +0300

    0.4: add more details to participants statistics

--- kernel/handlers/Participants.php
+++ kernel/handlers/Participants.php
@@ -33,18 +33,28 @@ class Participants implements WorksheetHandler
         if (!empty($data)) {
 
             $worksheet->setCellValue('A1', 'ID');
-            $worksheet->setCellValue('B1', 'E-mail');
+            $worksheet->setCellValue('B1', 'Фамилия');
             $worksheet->setCellValue('C1', 'Имя');
-            $worksheet->setCellValue('D1', 'Фамилия');
+            $worksheet->setCellValue('D1', 'Отчество');
+            $worksheet->setCellValue('E1', 'E-mail');
+            $worksheet->setCellValue('F1', 'Номер телефона');
+            $worksheet->setCellValue('G1', 'Дата рождения');
+            $worksheet->setCellValue('H1', 'Организация');
+            $worksheet->setCellValue('I1', 'Специальность');
 
             $i = 2;
 
             foreach ($data as $user_id => $userdata) {
 
                 $worksheet->setCellValue('A'.$i, $user_id);
-                $worksheet->setCellValue('B'.$i, $userdata['email']);
-                $worksheet->setCellValue('C'.$i, $userdata['first_name']);
-                $worksheet->setCellValue('D'.$i, $userdata['last_name']);
+                $worksheet->setCellValue('B'.$i, $userdata['Surname']);
+                $worksheet->setCellValue('C'.$i, $userdata['Name']);
+                $worksheet->setCellValue('D'.$i, $userdata['LastName']);
+                $worksheet->setCellValue('E'.$i, $userdata['email']);
+                $worksheet->setCellValue('F'.$i, $userdata['phone']);
+                $worksheet->setCellValue('G'.$i, $userdata['Date_of_Birth']);
+                $worksheet->setCellValue('H'.$i, $userdata['Organization']);
+                $worksheet->setCellValue('I'.$i, $userdata['Specialty']);
 
                 $i += 1;
 
