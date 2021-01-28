4534bfd4e5e3462d65145b702b8f62f6e12c22a1
Dmitry Shumilin <infernozzz@yandex.ru>
2021-01-28 10:45:38 +0300

    update titles form

--- event-platform-statistics-titles.php
+++ event-platform-statistics-titles.php
@@ -43,11 +43,33 @@ if (!empty($eps_titles_status)) echo $eps_titles_status;
 ?>
     <form action="" method="post" class="eps-titles-form mx-auto">
         <h4 class="text-center mb-4">Добавить новый элемент программы:</h4>
-        <div class="form-group mb-2">
+        <div class="form-group mb-3">
             <input type="text" class="form-control" id="eps-titles-header" name="eps-titles-header" placeholder="Введите заголовок">
         </div>
-        <div class="row">
-            
+        <p>Дата и время начала:</p>
+        <div class="row mb-3">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <input type="date" class="form-control" id="eps-titles-start-date" name="eps-titles-start-date">
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <input type="time" class="form-control" id="eps-titles-start-time" name="eps-titles-start-time">
+            </div>
+        </div>
+        <p>Дата и время конца:</p>
+        <div class="row mb-3">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <input type="date" class="form-control" id="eps-titles-end-date" name="eps-titles-end-date">
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <input type="time" class="form-control" id="eps-titles-end-time" name="eps-titles-end-time">
+            </div>
+        </div>
+        <div class="form-group mb-3">
+            <input type="checkbox" class="form-check-input" id="eps-titles-nmo" name="eps-titles-nmo" value="true">
+            <label for="eps-titles-nmo">Нужно учитывать в статистике для НМО</label>
+        </div>
+        <div class="form-group text-center">
+            <button type="submit" class="btn btn-primary" id="eps-titles-form-submit" disabled="true">Сохранить</button>
         </div>
     </form>
 </div>
