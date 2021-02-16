41eefde48336236db6323796b65265e6a4d27aae
Dmitry Shumilin <infernozzz@yandex.ru>
2021-02-16 14:36:01 +0300

    1.0.7: add some methods to Visits class

--- event-platform-statistics.php
+++ event-platform-statistics.php
@@ -3,7 +3,7 @@
  * Plugin Name: Event Platform Statistics
  * Plugin URI: https://github.com/drnoisier/event-platform-statistics
  * Description: Плагин, реализующий сбор статистики на площадке мероприятий.
- * Version: 1.0.6
+ * Version: 1.0.7
  * Author: Дмитрий Шумилин
  * Author URI: mailto://ds@brandpro.ru
  */
--- kernel/Visits.php
+++ kernel/Visits.php
@@ -28,6 +28,13 @@ class Visits
 
     }
 
+    /**
+     * Create a table.
+     * 
+     * @return void
+     * 
+     * @throws EPStatistics\Exceptions\VisitsException
+     */
     public function tableCreate() : void
     {
 
@@ -45,6 +52,131 @@ class Visits
             'Creating table failure.',
             -50
         );
+
+    }
+
+    /**
+     * Add a visit entry in the table.
+     * 
+     * @param string $url
+     * URL of the page was visited.
+     * 
+     * @param int $user_id
+     * User ID cannot be less than 1.
+     * 
+     * @return bool
+     * 
+     * @throws EPStatistics\Exceptions\VisitsException
+     */
+    public function addVisit(string $url, int $user_id) : bool
+    {
+
+        if ($user_id < 1) throw new VisitsException(
+            'User ID cannot be less than 1.',
+            -51
+        );
+
+        date_default_timezone_set('Europe/Moscow');
+
+        if ($this->wpdb->insert(
+            $this->wpdb->prefix.$this->table,
+            [
+                'user_id' => $user_id,
+                'page_url' => $url,
+                'datetime' => date("Y-m-d H:i:s")
+            ],
+            ['%d', '%s', '%s']
+        ) === false) return false;
+        else return true;
+
+    }
+
+    /**
+     * Get all visits.
+     * 
+     * @return array
+     * 
+     * @throws EPStatistics\Exceptions\VisitsException
+     */
+    public function getVisits() : array
+    {
+
+        $result = [];
+
+        $select = $this->wpdb->get_results(
+            "SELECT t.user_id, t.page_url, t.datetime
+                FROM ".$this->dbname.".".$this->wpdb->prefix.$this->table." AS t",
+            ARRAY_A
+        );
+
+        if (is_array($select)) {
+
+            if (!empty($select)) $result = $select;
+
+        } else throw new VisitsException(
+            'Getting visits failure.',
+            -52
+        );
+
+        return $result;
+
+    }
+
+    /**
+     * Getting visits ordered by users.
+     * 
+     * @return array
+     */
+    public function getVisitsByUsers() : array
+    {
+
+        $result = [];
+
+        $visits = $this->getVisits();
+
+        if (!empty($visits)) {
+
+            foreach ($visits as $entry) {
+
+                $result[$entry['user_id']][] = [
+                    'page_url' => $entry['page_url'],
+                    'datetime' => $entry['datetime']
+                ];
+
+            }
+
+        }
+
+        return $result;
+
+    }
+
+    /**
+     * Getting visits ordered by pages.
+     * 
+     * @return array
+     */
+    public function getVisitsByPages() : array
+    {
+
+        $result = [];
+
+        $visits = $this->getVisits();
+
+        if (!empty($visits)) {
+
+            foreach ($visits as $entry) {
+
+                $result[$entry['page_url']][] = [
+                    'user_id' => $entry['user_id'],
+                    'datetime' => $entry['datetime']
+                ];
+
+            }
+
+        }
+
+        return $result;
 
     }
 
