b8dbb8ea4f9eddca247a7916b621b9f98adc47c4
Dmitry Shumilin <infernozzz@yandex.ru>
2021-03-23 15:16:55 +0300

    1.7.7: add epsDisplayNoneSanitizer, fix presence effect button hiding

--- js/titles-client.js
+++ js/titles-client.js
@@ -97,7 +97,10 @@ function epsButtonHide(list_name)
     {
         if (buttons[i].getAttribute('eps-peb-list') == list_name)
         {
-            if (buttons[i].hasAttribute('style')) buttons[i].setAttribute('style', buttons[i].getAttribute('style')+' display: none;')
+            if (buttons[i].hasAttribute('style')) buttons[i].setAttribute(
+                'style',
+                epsDisplayNoneSanitizer(buttons[i].getAttribute('style'))+' display: none;'
+            )
             else buttons[i].setAttribute('style', 'display: none;')
 
             break
@@ -113,37 +116,42 @@ function epsButtonOpen(list_name)
     {
         if (buttons[i].getAttribute('eps-peb-list') == list_name)
         {
-            if (buttons[i].hasAttribute('style'))
-            {
-                const stylesArr = buttons[i].getAttribute('style').split(';')
-
-                let stylesStr = ''
-
-                let style = ''
-                let entity
-
-                for (let c = 0; c < stylesArr.length; c++)
-                {
-                    style = stylesArr[i].trim()
-
-                    entity = style.split(':')
-
-                    if (entity.length == 2)
-                    {
-
-                        if (entity[0].trim() != 'display' &&
-                            entity[1].trim() != 'none')
-                        {
-                            if (stylesStr == '') stylesStr = style+';'
-                            else stylesStr = stylesStr+' '+style+';'
-                        }
-                    }
-                }
-
-                buttons[i].setAttribute('style', stylesStr)
-            }
+            if (buttons[i].hasAttribute('style')) buttons[i].setAttribute(
+                'style',
+                epsDisplayNoneSanitizer(buttons[i].getAttribute('style'))
+            )
 
             break
         }
     }
+}
+
+function epsDisplayNoneSanitizer(styles)
+{
+    const stylesArr = styles.split(';')
+
+    let stylesStr = ''
+
+    let style = ''
+    let entity
+
+    for (let i = 0; i < stylesArr.length; i++)
+    {
+        style = stylesArr[i].trim()
+
+        entity = style.split(':')
+
+        if (entity.length == 2)
+        {
+
+            if (entity[0].trim() != 'display' &&
+                entity[1].trim() != 'none')
+            {
+                if (stylesStr == '') stylesStr = style+';'
+                else stylesStr = stylesStr+' '+style+';'
+            }
+        }
+    }
+
+    return stylesStr
 }
